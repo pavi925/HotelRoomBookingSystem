798cb10daae31c21c2f07a3a930a4ff50868fc82
Pavithra <pavithraarumugam925@gmail.com>
2026-07-23 21:30:49 +0530

    Created Room entity class

---
+++ src/Room.java
@@ -0,0 +1,67 @@
+public class Room {
+
+    private int roomNo;
+    private String guestName;
+    private String roomType;
+    private String checkInDate;
+    private String status;
+
+    public Room(int roomNo, String guestName, String roomType, String checkInDate, String status) {
+        this.roomNo = roomNo;
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.checkInDate = checkInDate;
+        this.status = status;
+    }
+
+    public int getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNo=" + roomNo +
+                ", guestName='" + guestName + '\'' +
+                ", roomType='" + roomType + '\'' +
+                ", checkInDate='" + checkInDate + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
