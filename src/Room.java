8607dda7c33604f34b9655d987563d72668444e2
Pavithra <pavithraarumugam925@gmail.com>
2026-07-29 12:32:13 +0530

    Simplify Room.java output by displaying only room details

--- src/Room.java
+++ src/Room.java
@@ -6,58 +6,64 @@ public class Room {
     private String checkInDate;
     private String status;
 
-    public Room(int roomNo, String roomType,  String status) {
+    // Constructor
+    public Room(int roomNo, String roomType, String status) {
         this.roomNo = roomNo;
         this.roomType = roomType;
         this.status = status;
+        this.guestName = "-";
+        this.checkInDate = "-";
     }
 
+    // Getters
     public int getRoomNo() {
         return roomNo;
-    }
-
-    public void setRoomNo(int roomNo) {
-        this.roomNo = roomNo;
     }
 
     public String getGuestName() {
         return guestName;
     }
 
-    public void setGuestName(String guestName) {
-        this.guestName = guestName;
-    }
-
     public String getRoomType() {
         return roomType;
-    }
-
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
     }
 
     public String getCheckInDate() {
         return checkInDate;
     }
 
-    public void setCheckInDate(String checkInDate) {
-        this.checkInDate = checkInDate;
-    }
-
     public String getStatus() {
         return status;
+    }
+
+    // Setters
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate = checkInDate;
     }
 
     public void setStatus(String status) {
         this.status = status;
     }
-    // Guest details are stored internally but not displayed in room listings.
+
     @Override
     public String toString() {
-        return "Room{" +
-                "roomNo=" + roomNo +
-                ", roomType='" + roomType + '\'' +
-                ", status='" + status + '\'' +
-                '}';
+        return String.format(
+                "Room No : %-5d | Room Type : %-8s | Status : %s",
+                roomNo,
+                roomType,
+                status
+        );
     }
-    }
+}
